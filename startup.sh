53ef935fbced2b00eb71a8f1901423f0ae86151c
Anjan Mondal <anjan.gk.mondal@gmail.com>
2025-01-18 05:49:08 +0530

    Added logic for execution between two periods in a day

--- startup.sh
+++ startup.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-start_time="00:00"
-end_time="10:00"
-current_time=$(date +"%H:%M")
+morning_start_time="00:00"
+morning_end_time="10:00"
+evening_start_time="18:00"
+evening_end_time="23:59"
 
-# Check if the current time is within the range
-if [[ "$current_time" > "$start_time" && "$current_time" < "$end_time" ]]; then
-    # Command to run if the time is within the range
+
+
+current_time=$(date +"%H:%M")
+# Check if the current time is within the morning or evening range
+if [[ ("$current_time" > "$morning_start_time" && "$current_time" < "$morning_end_time") || 
+      ("$current_time" > "$evening_start_time" && "$current_time" < "$evening_end_time") ]]; then
     echo "Running command: Opening browser"
-    	export BROWSER='/mnt/c/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe'
-	sensible-browser
-   fi
+    export BROWSER='/mnt/c/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe'
+    sensible-browser
+else
+    echo "Current time does not fall within any defined range."
+fi
